5c9b416b3d14a1e87c0f646963c18194591d975c
angus350 <anguswong1225@gmail.com>
2023-03-05 16:29:30 -0800

    New structure

--- about.php
+++ about.php
@@ -1,13 +1,17 @@
+
 <!DOCTYPE html>
 <html>
     <head>
         <title>About me!</title>
         <meta name="description" content="">
         <meta name="author" content="">
-
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+        <link rel="stylesheet" href="css/home-page-layout.css">
     </head>
     <body>
-        
+        <?php
+            session_start();
+            include("navbar.php");
+        ?>
     </body>
 </html>
---
+++ navbar.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+            <title>Angus's Personal Website</title>
+            <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+            <link rel="stylesheet" href="css/home-page-layout.css">
+    </head>
+    <body>
+        <header>
+            <nav>
+                <!--Heading navigation list-->
+                <ul class="navbar">
+                    <li><a href="index.html">Home</a></li>
+                    <li><a href="about.php">About</a></li>
+                    <li><a href="project.php">Project</a></li>
+                    <li><a href="volunteer.php">Volunteer</a></li>
+                    <li><a href="photo.php">Photo album</a></li>
+                </ul>
+            </nav>
+            <hr />
+        </header>
+    </body>
+</html>
