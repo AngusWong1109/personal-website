a7a75c420833a398dbc7050ed81d3970698fcfc4
Angus Wong <anguswong1225@gmail.com>
2024-04-22 14:34:44 -0700

    Add background video

--- database.js
+++ database.js
@@ -1,8 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
@@ -13,9 +11,14 @@ const firebaseConfig = {
   storageBucket: "personal-website-24fed.appspot.com",
   messagingSenderId: "354757116436",
   appId: "1:354757116436:web:29d71f58f008fac2d29d01",
-  measurementId: "G-4BHXP1Z81B"
+  measurementId: "G-4BHXP1Z81B",
+  databaseURL: "https://personal-website-24fed-default-rtdb.firebaseio.com/"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+const database = getDatabase(app)
+
+function addProject(title, picsLink, organisation, startDate, endDate, description, codeLink, websiteLink){
+    
+}
